d990286c490429578163270345f70751d840de67
Cesar Augusto Dos Santos <cesar140904@gmail.com>
2025-06-03 16:32:01 -0300

    Classe Categoria

---
+++ src/main/java/modelo/Categoria.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Categoria {
+
+    private int idCategoria;
+    private String nome;
+    private String tamanho;
+    private String embalagem;
+
+    public Categoria() {
+    }
+
+    public Categoria(int idCategoria, String nome, String tamanho, String embalagem) {
+        this.idCategoria = idCategoria;
+        this.nome = nome;
+        this.tamanho = tamanho;
+        this.embalagem = embalagem;
+    }
+
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTamanho() {
+        return tamanho;
+    }
+
+    public void setTamanho(String tamanho) {
+        this.tamanho = tamanho;
+    }
+
+    public String getEmbalagem() {
+        return embalagem;
+    }
+
+    public void setEmbalagem(String embalagem) {
+        this.embalagem = embalagem;
+    }
+}
