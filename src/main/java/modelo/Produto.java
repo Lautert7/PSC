315c44c1e33d987ecf7aa99dede4fb5d5fe5c987
Cesar Augusto Dos Santos <cesar140904@gmail.com>
2025-06-03 15:43:15 -0300

    Update Produto.java

--- src/main/java/modelo/Produto.java
+++ src/main/java/modelo/Produto.java
@@ -1,6 +1,89 @@
+package model;
 
-package modelo;
-
-public class Produto {
+pubclic class Produto {
+    private int idProduto;
+    private String nome;
+    private double preco;
+    private String unidade;
+    private int quantidadeEstoque;
+    private int quantidadeMinima;
+    private int quantidadeMaxima;
+    private String categoria;
     
+    public Produto(int idProduto, String nome, double preco, String unidade,
+                   int quantidadeEstoque, int quantidadeMinima, int quantidadeMaxima, String categoria) {
+        this.idProduto = idProduto;
+        this.nome = nome;
+        this.preco = preco;
+        this.unidade = unidade;
+        this.quantidadeEstoque = quantidadeEstoque;
+        this.quantidadeMinima = quantidadeMinima;
+        this.quantidadeMaxima = quantidadeMaxima;
+        this.categoria = categoria;
+    }
+
+    // Getters e Setters
+    public int getIdProduto() {
+        return idProduto;
+    }
+
+    public void setIdProduto(int idProduto) {
+        this.idProduto = idProduto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getUnidade() {
+        return unidade;
+    }
+
+    public void setUnidade(String unidade) {
+        this.unidade = unidade;
+    }
+
+    public int getQuantidadeEstoque() {
+        return quantidadeEstoque;
+    }
+
+    public void setQuantidadeEstoque(int quantidadeEstoque) {
+        this.quantidadeEstoque = quantidadeEstoque;
+    }
+
+    public int getQuantidadeMinima() {
+        return quantidadeMinima;
+    }
+
+    public void setQuantidadeMinima(int quantidadeMinima) {
+        this.quantidadeMinima = quantidadeMinima;
+    }
+
+    public int getQuantidadeMaxima() {
+        return quantidadeMaxima;
+    }
+
+    public void setQuantidadeMaxima(int quantidadeMaxima) {
+        this.quantidadeMaxima = quantidadeMaxima;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
